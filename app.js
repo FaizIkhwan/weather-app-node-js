b89f6f05e8dab931342b73898734502dada2c246
Faiz Ikhwan <faiz.ikhwan@hexainitiative.com>
2020-03-28 14:24:35 +0800

    [] print body

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const request = require('request')
 
 const url = 'https://api.darksky.net/forecast/49322f6afe96a4c8340860bc83a5a7d6/37.8267,-122.4233'
 
-request({ url: url }, (error, response) => {
-    const data = JSON.parse(response.body)
-    console.log(data.currently)
+request({ url: url, json: true }, (error, response) => {
+    console.log(response.body.currently)
 })
