fb3f907e5fd0a62c77396ccf15db22437806ba01
Faiz Kwan <faizikhwantop@gmail.com>
2020-03-29 19:47:27 +0800

    [] error handling.

--- app.js
+++ app.js
@@ -3,5 +3,9 @@ const request = require('request')
 const url = 'https://api.darksky.net/forecast/49322f6afe96a4c8340860bc83a5a7d6/37.8267,-122.4233'
 
 request({ url: url, json: true }, (error, response) => {
-    console.log(response.body.currently)
+    if (error) {
+        console.log(error)
+    } else {
+        console.log(response.body)
+    }    
 })
